bf5abc0fcd025cdf27e8b332d4cad9648c05fb7e
Esteban <esteban@basischile.cl>
2025-01-03 12:31:59 -0300

    Update footer.php

--- Shared/footer.php
+++ Shared/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div style="background-color: #4CAF50; color: white; text-align: center; padding: 20px; position: relative; bottom: 0; width: 100%; box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);">
+    <div style="background-color: #4CAF50; color: white; text-align: center; padding: 10px; position: relative; bottom: 0; width: 100%; box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);">
         <p>&copy; 2024 IDAHUE. Todos los derechos reservados.</p>
     </div>
 </footer>
